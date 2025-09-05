6bbfdf2a18042674a4119cf29a320d6c09333e5e
Blindeen <kuba.szmajda@gmail.com>
2025-09-05 17:52:05 +0200

    prettify files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Input } from "@/components/ui/input";
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
-  return (
-    <div className="h-full flex items-center justify-center px-8">
-      <Input placeholder="Enter URL" />
-    </div>
-  );
+    return (
+        <div className='h-full flex items-center justify-center px-8'>
+            <Input placeholder='Enter URL' />
+        </div>
+    );
 }
