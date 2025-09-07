41566476e26c1341c496d3cd32dcb55ee2bc1935
Blindeen <kuba.szmajda@gmail.com>
2025-09-07 16:04:31 +0200

    display a short url, allow copying a url straight from input, display tooltip on input hover

--- src/features/url-shortener/CreateShortUrlForm.tsx
+++ src/features/url-shortener/CreateShortUrlForm.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -7,6 +9,11 @@ import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { ChevronRightIcon } from 'lucide-react';
 import {
     Form,
@@ -26,6 +33,9 @@ const formSchema = z.object({
 });
 
 export function CreateShortUrlForm() {
+    const [shortenedUrl, setShortenedUrl] = useState<string | undefined>(
+        undefined
+    );
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -35,7 +45,9 @@ export function CreateShortUrlForm() {
 
     const onSubmit = async (formData: z.infer<typeof formSchema>) => {
         try {
-            const response = await shortenUrl(formData.url);
+            const { url } = await shortenUrl(formData.url);
+            setShortenedUrl(url);
+            form.reset();
             toast.success('URL shortened successfully!');
         } catch {
             toast.error('Failed to shorten URL. Please try again.');
@@ -43,36 +55,54 @@ export function CreateShortUrlForm() {
     };
 
     return (
-        <Form {...form}>
-            <form
-                className='flex h-16 w-5/6 gap-x-2 md:w-3/5 lg:w-4/12'
-                onSubmit={form.handleSubmit(onSubmit)}
-            >
-                <FormField
-                    control={form.control}
-                    name='url'
-                    render={({ field }) => (
-                        <FormItem className='flex-1'>
-                            <FormControl>
-                                <Input
-                                    className='bg-white'
-                                    placeholder='Enter a URL'
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button
-                    className='cursor-pointer'
-                    variant='secondary'
-                    size='icon'
-                    type='submit'
+        <div className='flex h-28 w-5/6 flex-col gap-y-2 md:w-3/5 lg:w-4/12'>
+            <Form {...form}>
+                <form
+                    className='flex gap-x-2'
+                    onSubmit={form.handleSubmit(onSubmit)}
                 >
-                    <ChevronRightIcon />
-                </Button>
-            </form>
-        </Form>
+                    <FormField
+                        control={form.control}
+                        name='url'
+                        render={({ field }) => (
+                            <FormItem className='h-16 flex-1'>
+                                <FormControl>
+                                    <Input
+                                        className='bg-white'
+                                        placeholder='Enter a URL'
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button
+                        className='cursor-pointer'
+                        variant='secondary'
+                        size='icon'
+                        type='submit'
+                    >
+                        <ChevronRightIcon />
+                    </Button>
+                </form>
+            </Form>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    {shortenedUrl && (
+                        <Input
+                            className='cursor-pointer bg-white'
+                            placeholder='Shortened URL will appear up here'
+                            value={shortenedUrl}
+                            onClick={() => console.log('clicked')}
+                            readOnly
+                        />
+                    )}
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>Click to copy the shortened URL</p>
+                </TooltipContent>
+            </Tooltip>
+        </div>
     );
 }
