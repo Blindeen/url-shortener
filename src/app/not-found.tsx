b80fc063858cc8e727fc90eecc1ead0fcf140155
Blindeen <kuba.szmajda@gmail.com>
2025-09-09 22:55:41 +0200

    create separate 404 component, provide separate 404 page for `[slug]` and entire application

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,6 @@ import { NotFound } from '@/components/ui/not-found';
 
 export default function NotFoundPage() {
     return (
-        <NotFound description="Oops! The short URL doesn't exist or is corrupted" />
+        <NotFound description="Oops! The page you're looking for doesn't exist" />
     );
 }
